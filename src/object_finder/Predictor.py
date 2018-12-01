64e3f158ce813880b857f521364c7f75dff518f8
Svetlin <sxs1307@student.bham.ac.uk>
2018-12-01 18:13:52 +0000

    Added keras stuff ro predictor

--- src/object_finder/Predictor.py
+++ src/object_finder/Predictor.py
@@ -1,17 +1,26 @@
-from net.build import TFNet
-import cv2
+from keras.models import Sequential, load_model
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+
 import ImageTaker
 
 class Predictor(object):
     def __init__(self):
         self.image_taker = ImageTaker.ImageTaker()
-        #self.tfnet = TFNet(<options_from_yolo>)
+        self.use_file = False 
          
     def predict(self):
-        frame = self.image_taker.capture()
-        cv2.imshow("frame",frame)
-        cv2.waitKey(0) 
-        cv2.imwrite("image.jpg",frame)
+        model = load_model("ir_cnn_1.hdf5")
         
-        
+        if self.use_file:
+            img = load_img("./balls/ball134.jpg")
+        else:
+            img = self.image_taker.capture()   
+        img = img.resize((150,150))
+        x = img_to_array(img)
+        print(x.shape)
+        x = x.reshape((1,) + x.shape)
+        print(x.shape)
+        cl = model.predict_classes(x)
+
+        print(cl)
 
